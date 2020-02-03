008007781fd44d3c08c611d65824db32297d8ee7
nightelfamber <nightelfamber@gmail.com>
2020-02-03 15:35:21 +0800

    错误

---
+++ src/views/home/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id='home'>
+    <nav-bar class="home-nav">
+      <div slot="center">购物节</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar';
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    },
+    data() {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+<style scoped>
+</style>
